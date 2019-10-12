8758095744dd04b84a5d429def3dd4c98eb944b4
André Martins <andre@basedigital.pt>
2019-10-12 15:27:00 +0100

    Player score done but not added to the interface

---
+++ src/org/academiadecodigo/vimdiesels/gameprojectac/Score.java
@@ -0,0 +1,21 @@
+package org.academiadecodigo.vimdiesels.gameprojectac;
+
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Text;
+
+public class Score {
+
+    private int points;
+    Text score;
+
+    public Score() {
+        score = new Text(30, 30, "000");
+        score.setColor(Color.CYAN);
+        score.draw();
+    }
+
+    public void setScore(int points) {
+        score.setText( String.valueOf( this.points += points ) );
+    }
+
+}
